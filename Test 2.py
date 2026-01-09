e305739e72aedda617d90689e266b20739617355
Rown <ronanw135@gmail.com>
2026-01-09 11:16:27 -0330

    Fix syntax errors and improve repeat function

--- Test 2.py
+++ Test 2.py
@@ -1,4 +1,5 @@
 import streamlit as st
+time == "100000000"
 
 def print_x_block(times):
     x_row = "X" * 10000000  # Much smaller row
@@ -9,6 +10,6 @@ st.title("X Printer 3000 🚀")
 
 if st.button("Unleash the Xs"):
     print("your cooked womp womp maka maka")
-    def repeat():
+    for _ in range(time)
         print_x_block(10000000)  # Reasonable number of rows
-        repeat()
+
