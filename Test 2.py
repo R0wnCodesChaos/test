03a0a00e7631e883b6eb6f04b9e46e92b17dd5d3
Rown <ronanw135@gmail.com>
2026-01-09 11:22:38 -0330

    Reduce number of 'X' rows printed

--- Test 2.py
+++ Test 2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def print_x_block(times):
-    x_row = "X" * 10000000  # Much smaller row
+    x_row = "X" * 1000000  # Much smaller row
     for _ in range(times):
         st.text(x_row)
         for _ in range(times):
@@ -11,5 +11,5 @@ st.title("X Printer 3000 🚀")
 
 if st.button("Unleash the Xs"):
     print("your cooked womp womp maka maka")
-    print_x_block(10000000)  # Reasonable number of rows
+    print_x_block(1000000)  # Reasonable number of rows
 
